d1995dcfc34086cd27bbcf7d99a4d3cca263257b
Mikko Partio <mikko.partio@fmi.fi>
2021-10-04 13:16:28 +0300

    Param name changed and precision reduced

--- functional-tests/pop/smartmet.sh
+++ functional-tests/pop/smartmet.sh
@@ -10,7 +10,7 @@ rm -f POP-PRCNT*.grib2
 
 $HIMAN -d 5 -j 1 -f pop_smartmet.json --no-cuda source.grib
 
-grib_compare -A 1e-5 POP-PRCNT_height_0_polster_255_280_0_044.grib2 result.grib 
+grib_compare -A 1e-5 POP-0TO1_height_0_polster_255_280_0_044.grib2 result.grib
 
 if [ $? -eq 0 ];then
   echo pop/smartmet success on CPU!
