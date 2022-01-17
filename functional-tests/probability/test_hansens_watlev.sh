de9faabf3d6cdc8d05113f5d01c50245ad105e4c
Mikko Partio <mikko.partio@fmi.fi>
2022-01-17 09:24:02 +0200

    qddifference does not work anymore with non-gridded data

--- functional-tests/probability/test_hansens_watlev.sh
+++ functional-tests/probability/test_hansens_watlev.sh
@@ -6,11 +6,17 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan-bin/build/debug/himan"
 fi
 
-rm -f hansens_watlev.fqd
+rm -f hansens_watlev.fqd a b
 
 $HIMAN -d 5 -f hansens_watlev.json --no-cuda hansens_watlev_source.csv
 
-qddifference -e 1 hansens_watlev.json.fqd hansens_watlev_result.fqd
+# Note: qddifference does not work anymore
+# qddifference -e 1 hansens_watlev.json.fqd hansens_watlev_result.fqd
+
+qdstat hansens_watlev.json.fqd > a
+qdstat hansens_watlev_result.fqd > b
+
+diff -u a b
 
 if [ $? -eq 0 ]; then
 	echo probability/hansens_watlev success on CPU!
@@ -18,3 +24,5 @@ else
 	echo probability/hansens_watlev failed on CPU!
 	exit 1
 fi
+
+rm -f hansens_watlev.fqd a b
