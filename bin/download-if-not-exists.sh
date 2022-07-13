56fb697b6b39a2b7773491adccfe4444f9fc2296
Mikko Partio <mikko.partio@fmi.fi>
2022-07-13 20:15:04 +0300

    Add --retry 1 to mitigate flaky network connection to lake.fmi.fi

--- bin/download-if-not-exists.sh
+++ bin/download-if-not-exists.sh
@@ -5,5 +5,5 @@ data=$1
 if [ ! -f "$data" ]; then
   echo "data $data not present, copying it from lake.fmi.fi"
   set -ex
-  curl https://lake.fmi.fi/himan-tests-source-data/$data -o $data
+  curl --retry 1 https://lake.fmi.fi/himan-tests-source-data/$data -o $data
 fi
