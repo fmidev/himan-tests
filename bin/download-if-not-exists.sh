c33cb0147fdb5675ac713f9b94c2aafa55b250b7
Mikko Partio <mikko.partio@fmi.fi>
2023-01-12 09:20:20 +0200

    Manual retry

--- bin/download-if-not-exists.sh
+++ bin/download-if-not-exists.sh
@@ -6,4 +6,8 @@ if [ ! -f "$data" ]; then
   echo "data $data not present, copying it from lake.fmi.fi"
   set -ex
   curl --retry 1 --keepalive-time 2 https://lake.fmi.fi/himan-tests-source-data/$data -o $data
+  if [ $? -ne 0 ]; then
+    # retry
+    curl --retry 1 --keepalive-time 2 https://lake.fmi.fi/himan-tests-source-data/$data -o $data
+  fi
 fi
