6bc8c55374c43996ebefb6e2a1f9e030f4896595
Mikko Partio <mikko.partio@fmi.fi>
2023-02-21 15:06:05 +0200

    Updateded grib metadata

--- functional-tests/pot/ecmwf.sh
+++ functional-tests/pot/ecmwf.sh
@@ -6,13 +6,13 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan"
 fi
 
-rm -f POT-PRCNT*.grib
+rm -f POT-PRCNT*.grib2
 
 ../../bin/download-if-not-exists.sh pot_source_ecmwf.grib.bz2
 
 $HIMAN -d 5 -j 4 -f pot_ec.json --no-cuda pot_source_ecmwf.grib.bz2
 
-grib_compare POT-PRCNT_height_0_ll_3600_1801_0_003.grib result.grib 
+grib_compare POT-PRCNT_height_0_ll_3600_1801_0_003.grib2 result.grib 
 
 if [ $? -eq 0 ];then
   echo pot/ecmwf success on CPU!
@@ -21,4 +21,4 @@ else
   exit 1
 fi
 
-rm -f POT-PRCNT*.grib
+rm -f POT-PRCNT*.grib2
