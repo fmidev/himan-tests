c808615dc156f322280b4c92b05c3a6271f2882d
Mikko Partio <mikko.partio@fmi.fi>
2023-05-17 08:47:39 +0300

    Update test for meps gust to use only grib2 as input. No output data changes

--- functional-tests/gust/meps.sh
+++ functional-tests/gust/meps.sh
@@ -6,13 +6,13 @@ if [ -z "$HIMAN" ]; then
 	export HIMAN="../../himan"
 fi
 
-rm -f FFG2-MS_height_10_lcc_739_949_0_004_4_0.grib2
+rm -f FFG2-MS*
 
 ../../bin/download-if-not-exists.sh gust_meps_source.grib
 
-$HIMAN -d 4 -j 1 -f gust_meps.json -t grib --no-cuda gust_meps_source.grib 
+$HIMAN -d 4 -j 1 -f gust_meps.json -t grib2 --no-cuda gust_meps_source.grib 
 
-grib_compare FFG2-MS_height_10_lcc_739_949_0_004_4_0.grib2 meps_result.grib
+grib_compare FFG2-MS_height_10_lcc_949_1069_0_004_4_0.grib2 meps_result.grib
 
 if [ $? -eq 0 ];then
   echo gust/meps success on CPU!
@@ -21,22 +21,4 @@ else
   exit 1
 fi
 
-if [ $(/sbin/lsmod | egrep -c "^nvidia") -gt 0 ]; then
-
-  rm -f FFG2-MS_height_10_lcc_739_949_0_004_4_0.grib2
-
-  $HIMAN -d 4 -j 1 -f gust_meps.json -t grib gust_meps_source.grib
-
-  grib_compare -A 0.2 FFG2-MS_height_10_lcc_739_949_0_004_4_0.grib2 meps_result.grib
-
-  if [ $? -eq 0 ];then
-    echo gust/meps success on GPU!
-  else
-    echo gust/meps failed on GPU
-    exit 1
-  fi
-
-fi
-
-rm -f FFG2-MS_height_10_lcc_739_949_0_004_4_0.grib2
-
+rm -f FFG2-MS*
