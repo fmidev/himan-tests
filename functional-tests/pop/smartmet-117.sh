2de5476fc0dc7f67c60eec40b7e48707dc7ced94
Mikko Partio <mikko.partio@fmi.fi>
2023-10-06 13:53:40 +0300

    Somekind of mishap with test; redo

--- functional-tests/pop/smartmet-117.sh
+++ functional-tests/pop/smartmet-117.sh
@@ -10,7 +10,7 @@ rm -f POP-PRCNT*.grib2
 
 $HIMAN -d 5 -j 1 -f smartmet-117.json --no-cuda source-117.grib
 
-grib_compare -A 1e-5 POP-0TO1_height_0_polster_255_280_0_044.grib2 result-117.grib
+grib_compare -A 1e-5 POP-0TO1_height_0_polster_255_280_0_117.grib2 result-117.grib
 
 if [ $? -eq 0 ];then
   echo pop/smartmet-117 success on CPU!
