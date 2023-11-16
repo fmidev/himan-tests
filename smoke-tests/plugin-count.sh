86ced0b3a6c15b33267aa38682911bd5ec111477
Mikko Partio <mikko.partio@fmi.fi>
2023-11-16 13:08:53 +0200

    Verbose logging

--- smoke-tests/plugin-count.sh
+++ smoke-tests/plugin-count.sh
@@ -1,6 +1,7 @@
+set -x
 cnt=$($HIMAN -l | grep -c Plugin)
 
 if [ $cnt -lt 48 ]; then
-    echo "plugin count was less than expected"
+    echo "plugin count was less than expected: $cnt vs 48"
     exit 1
 fi
