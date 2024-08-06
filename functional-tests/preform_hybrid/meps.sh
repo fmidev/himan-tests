9481566ff47f982c9236d38f8a75dde014c049cf
Mikko Partio <mikko.partio@fmi.fi>
2024-08-06 14:37:44 +0300

    Add test for meps summer precipitation

--- functional-tests/preform_hybrid/meps.sh
+++ functional-tests/preform_hybrid/meps.sh
@@ -23,4 +23,22 @@ else
   echo preform_hybrid/meps success on CPU
 fi
 
-rm -f PRECFORM2*lcc*grib2
+rm -f PRECFORM2*lcc*grib2 meps-preform-summer.grib2
+
+source_data=preform_hybrid_meps_source2.grib
+
+../../bin/download-if-not-exists.sh $source_data
+
+$HIMAN -d 4 -f meps-preform-summer.json --no-cuda -s meps-stat $source_data -t grib2
+
+grib_compare meps-preform-summer.grib2 meps-summer-result.grib
+
+if [ $? -ne 0 ];then
+  echo preform_hybrid/meps-summer failed on CPU
+  exit 1
+else
+  echo preform_hybrid/meps-sumer success on CPU
+fi
+
+rm -f meps-preform-summer.grib2
+
