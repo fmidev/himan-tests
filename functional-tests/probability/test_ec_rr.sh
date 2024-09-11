834637e2237457678f27a8f194433b52a9559a7d
Mikko Partio <mikko.partio@fmi.fi>
2024-09-11 14:14:44 +0300

    eccodes 2.35

--- functional-tests/probability/test_ec_rr.sh
+++ functional-tests/probability/test_ec_rr.sh
@@ -10,14 +10,14 @@ rm ./PROB-RR24-1_ground_0_rll_331_289_0_0{12,24}*.grib2
 
 $HIMAN -d 4 -f ec_rr.json --no-cuda ec_rr_source.grib
 
-grib_compare -A 0.001 ./PROB-RR24-1_ground_0_rll_331_289_0_012_5.grib2  ec_rr_12_result.grib
+grib_compare -b referenceValue -A 0.001 ./PROB-RR24-1_ground_0_rll_331_289_0_012_5.grib2  ec_rr_12_result.grib
 
 if [ $? -ne 0 ]; then
 	echo probability/ec_rr failed on CPU!
 	exit 1
 fi
 
-grib_compare -A 0.001 ./PROB-RR24-1_ground_0_rll_331_289_0_024_5.grib2 ec_rr_24_result.grib
+grib_compare -b referenceValue -A 0.001 ./PROB-RR24-1_ground_0_rll_331_289_0_024_5.grib2 ec_rr_24_result.grib
 
 if [ $? -eq 0 ]; then
 	echo probability/ec_rr success on CPU!
