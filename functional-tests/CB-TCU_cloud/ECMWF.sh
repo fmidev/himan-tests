5ebb1f09bf7da4f0c9b6647549fd0a75af7f6913
tackandr <andreas.tack@fmi.fi>
2024-11-01 11:37:41 +0200

    update CBTCU test

--- functional-tests/CB-TCU_cloud/ECMWF.sh
+++ functional-tests/CB-TCU_cloud/ECMWF.sh
@@ -12,7 +12,7 @@ rm -f CBTCU*.grib
  
 $HIMAN -d 5 -f CB-TCU_cloud_ec.json -t grib --no-cuda CB-TCU_cloud_source_ec.grib
 
-grib_compare -A 0.1 CBTCU-FL_height_0_rll_661_576_0_001.grib result_ec.grib
+grib_compare -A 0.1 CBTCU-FL_height_0_rll_661_576_0_005.grib result_ec.grib
 
 if [ $? -eq 0 ];then
   echo CB-TCU/ECMWF success on CPU!
@@ -21,4 +21,4 @@ else
   exit 1
 fi
 
-rm -f CBTCU*.grib
+#rm -f CBTCU*.grib
