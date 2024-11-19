5e732cb1c772aa95eb1a6f2d4f66a017323d3ba1
Mikko Partio <mikko.partio@fmi.fi>
2024-11-19 11:05:21 +0200

    Revert "Use --retry-all-errors"
    This reverts commit 5161eb5636e9026e881c95e62c45d3187bdd76f3.

--- bin/download-if-not-exists.sh
+++ bin/download-if-not-exists.sh
@@ -5,5 +5,9 @@ data=$1
 if [ ! -f "$data" ]; then
   echo "data $data not present, copying it from lake.fmi.fi"
   set -ex
-  curl --retry 1 --retry-all-errors --keepalive-time 2 https://lake.fmi.fi/himan-tests-source-data/$data -o $data
+  curl --retry 1 --keepalive-time 2 https://lake.fmi.fi/himan-tests-source-data/$data -o $data
+  if [ $? -ne 0 ]; then
+    # retry
+    curl --retry 1 --keepalive-time 2 https://lake.fmi.fi/himan-tests-source-data/$data -o $data
+  fi
 fi
