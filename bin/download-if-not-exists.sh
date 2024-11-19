836a1fc614a942ee32d88db40b6971d023a40917
Mikko Partio <mikko.partio@fmi.fi>
2024-11-19 11:11:11 +0200

    Check file size

--- bin/download-if-not-exists.sh
+++ bin/download-if-not-exists.sh
@@ -5,9 +5,17 @@ data=$1
 if [ ! -f "$data" ]; then
   echo "data $data not present, copying it from lake.fmi.fi"
   set -ex
-  curl --retry 1 --keepalive-time 2 https://lake.fmi.fi/himan-tests-source-data/$data -o $data
-  if [ $? -ne 0 ]; then
+  url=https://lake.fmi.fi/himan-tests-source-data/$data
+  s3_size=$(curl -sI $url | grep -i Content-Length | awk '{print $2}' | tr -cd '[:print:]')
+
+  curl --retry 1 --keepalive-time 2 $url -o $data
+
+  file_size=$(stat -c %s $data)
+
+  if [ $? -ne 0 ] || [ $file_size -ne $s3_size ] ; then
     # retry
-    curl --retry 1 --keepalive-time 2 https://lake.fmi.fi/himan-tests-source-data/$data -o $data
+    echo "Download failed, retrying"
+    rm -f $data
+    curl --retry 1 --keepalive-time 2 $url -o $data
   fi
 fi
