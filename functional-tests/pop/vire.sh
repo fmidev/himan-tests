126e2f6f4360929f9b0763d71f690bdf7da4f04f
Mikko Partio <mikko.partio@fmi.fi>
2025-05-20 15:54:58 +0300

    Rename files

--- functional-tests/pop/vire.sh
+++ functional-tests/pop/vire.sh
@@ -8,14 +8,14 @@ fi
 
 rm -f POP-0TO1*.grib2
 
-$HIMAN -d 5 -j 1 -f pop_smartmet.json --no-cuda source.grib
+$HIMAN -d 5 -j 1 -f pop_vire.json --no-cuda source.grib
 
 grib_compare -A 1e-5 POP-0TO1_height_0_polster_765_840_0_044.grib2 result.grib
 
 if [ $? -eq 0 ];then
-  echo pop/smartmet success on CPU!
+  echo pop/vire success on CPU!
 else
-  echo pop/smartmet failed on CPU
+  echo pop/vire failed on CPU
   exit 1
 fi
 rm -f POP-0TO1*.grib2
